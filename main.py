41bc7a40e30546c6c72e040327b6472e4c6dcb2c
AzmainAhnaf <azmainahnaf2003@gmail.com>
2021-08-04 17:39:25 +0600

    Updated the skeleton

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-with open('vocabulary.txt', 'r') as rf:
-    with open('vocabulary.txt', 'a') as wf:
-        print("Welcome to Vocabulary Builder. Press enter to continue")
-        input()
 
-        while True:
-            print('Press (s) to see your current list')
-            print('press (c) to check if you desired is in your list or not')
-            print('Press (a) to add new words')
-            print('Press (d) to delete words')
-            print('Press (q) to quit\n')
+print("Welcome to Vocabulary Builder. Press enter to continue")
+input()
 
-            response = input('--> ')
-            if response == 's' or response == 'S':
-                for line in rf:
-                    print(line, end = "")
-                print("\n\n")
-            elif response == 'q' or response == "Q":
-                break
+while True:
+    print('Press (s) to see your current list')
+    print('press (c) to check if you desired is in your list or not')
+    print('Press (a) to add new words')
+    print('Press (d) to delete words')
+    print('Press (q) to quit\n')
+
+    response = input('--> ')
+    if response == 's' or response == 'S':
+        pass
+    
+    elif response == 'q' or response == 'Q':
+        break
+
+    elif response == 'c' or response == 'C':
+        pass
+
+    elif response == 'a' or response == 'A':
+        pass
+
+    elif response == 'd' or response == 'D':
+        pass
